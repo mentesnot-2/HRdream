75662d56550706e50fa7f17aed682ca564045406
mentesnot-2 <sibhatmentesnot@gmail.com>
2025-01-06 17:33:57 +0300

    some file correction

--- hrdream-frontend/app/dashboard/reflect/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-export default function ReflectPage() {
-
-    return (
-        <div className=" bg-white flex w-full">
-            <h2>Reflect</h2>
-        </div>
-    )
-}
--- hrdream-frontend/app/settings/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-export default function SettingsPage() {
-
-    return (
-        <div className=" bg-white flex w-full">
-            <h2>Settings</h2>
-        </div>
-    )
-}
