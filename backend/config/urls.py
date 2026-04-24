10395457873a5124a9a347f317396a245badcef6
mentesnot-2 <sibhatmentesnot@gmail.com>
2026-04-24 15:01:42 +0300

    feat(urls): register Department and Employee routers

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -21,4 +21,5 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("core.urls")),
+    path("api/v1/", include("directory.urls"))
 ]
